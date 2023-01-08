e36bdac41bf3fd8ff31c951967ecd20e238a4550
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-01-08 20:33:03 +0330

    Add files via upload

---
+++ 118.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+con = sqlite3.connect('info.db')
+cur = con.cursor()
+
+# cur.execute("CREATE TABLE customers (name VARCHAR(50),age INT, phone VARCHAR(50));")
+# cur.execute("INSERT INTO customers VALUES ('Arian',16,'09353334325');")
+# cur.execute("INSERT INTO customers VALUES ('Sara',24,'09213378325');")
+#
+# cur.execute("DELETE FROM customers;")  # Deletes every record in table
+# cur.execute("DROP TABLE customers;") # Deletes table
+cur.execute("SELECT * FROM customers;")
+records = cur.fetchall()
+# print(records)
+for i in records:
+    print(i[-1])
+
+con.commit()
+con.close()
+print('Done')
